7939a3ef74e99702ba15ee01a6b7119b0d7065e4
lbl8603 <49143209+lbl8603@users.noreply.github.com>
2024-06-02 22:24:52 +0800

    去除有误的代码

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -5,8 +5,6 @@ class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
-    windowFrame.width = 600;
-    windowFrame.height = 700;
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
